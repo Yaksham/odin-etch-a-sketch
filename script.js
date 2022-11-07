0c081b9e52f05d6846157a590646df30d789a9ba
Yaksham <82596143+Yaksham@users.noreply.github.com>
2022-11-07 14:30:54 +0530

    added gradient option

--- script.js
+++ script.js
@@ -21,6 +21,26 @@ function selectColor(number) {
     return `hsl(${hue},40%,65%)`;
 }
 
+function grad(block) {
+    let r = 3.9;
+    let g = -6.4;
+    let b = -13.6;
+    let rgb = (window.getComputedStyle(block).backgroundColor);
+    rgb = rgb.replace(/[^\d,]/g, '').split(',');
+    rgb_og = rgb;
+    for (let i = 0; i < 3; i++) {
+        rgb[i] = parseFloat(rgb[i]);
+    }
+    if (rgb[1] < 127 || rgb[2] < 80)
+        return rgb_og;
+    rgb[0] += r;
+    rgb[1] += g;
+    rgb[2] += b;
+    
+    rgb = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`
+    return rgb;
+}
+
 function toggle(arg) {
     const blocks = document.querySelectorAll('.block');
     blocks.forEach(block => {
@@ -29,13 +49,20 @@ function toggle(arg) {
             block.classList.remove('active');
             return;
         }
+        if (arg === 'gradient') {
+            block.style.backgroundColor = null;
+            block.classList.remove('active');
+        }
         block.addEventListener('mouseover', () => {
             if (arg === 'erase') {
                 block.style.backgroundColor = null;
                 block.classList.remove('active');
             }
             else if (arg === 'random') {
-                block.style.backgroundColor= selectColor(Math.floor(Math.random() * 999));
+                block.style.backgroundColor = selectColor(Math.floor(Math.random() * 999));
+            }
+            else if (arg === 'gradient') {
+                block.style.backgroundColor = grad(block);
             }
             else {
                 block.style.backgroundColor = null;
@@ -51,6 +78,7 @@ const draw = document.querySelector('#draw');
 const eraser = document.querySelector('#erase');
 const random = document.querySelector('#random');
 const reset = document.querySelector('#reset');
+const gradient = document.querySelector('#gradient');
 
 btn.addEventListener('click', () => {
     console.log('lol')
@@ -74,5 +102,6 @@ eraser.addEventListener('click', toggle.bind(null, 'erase'));
 draw.addEventListener('click', toggle.bind(null, 'draw'));
 random.addEventListener('click', toggle.bind(null, 'random'));
 reset.addEventListener('click', toggle.bind(null, 'reset'));
+gradient.addEventListener('click', toggle.bind(null, 'gradient'))
 
 create();
