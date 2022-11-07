9d6a69cb6f243ff982339d947a321e25febffc77
Yaksham <82596143+Yaksham@users.noreply.github.com>
2022-11-07 12:59:32 +0530

    added various buttons

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
-const div = document.createElement('div');
-div.classList.add('block');
-const divRow = document.createElement('div');
-divRow.classList.add('row');
-const main = document.querySelector('#main');
+function create(size=4) {
+    const div = document.createElement('div');
+    div.classList.add('block');
+    const divRow = document.createElement('div');
+    divRow.classList.add('row');
+    const main = document.querySelector('#main');
 
-size = 4;
-
-for (let i = 0; i < size; i++) {
-    main.appendChild(divRow.cloneNode());
-}
-document.querySelectorAll('.row').forEach(row => {
     for (let i = 0; i < size; i++) {
-        row.appendChild(div.cloneNode());
+        main.appendChild(divRow.cloneNode());
     }
-})
-
-const blocks = document.querySelectorAll('.block');
-
-// const btn = document.querySelector('#size');
-// btn.addEventListener('click', () => {
-//     size = prompt('Enter number of squares on one side');
-//     rows = document.querySelectorAll('.row');
-//     rows.forEach((row) => {
-//         row.parentNode.removeChild(row);
-//     })
-// })
-
-blocks.forEach(block => {
-    block.addEventListener('mouseover', () => {
-        block.classList.toggle('active');
+    document.querySelectorAll('.row').forEach(row => {
+        for (let i = 0; i < size; i++) {
+            row.appendChild(div.cloneNode());
+        }
     })
-    block.addEventListener('mouseout', () => {
-        block.classList.toggle('active');
+    toggle();
+}
+
+function selectColor(number) {
+    const hue = number * 137.508; // use golden angle approximation
+    return `hsl(${hue},40%,65%)`;
+}
+
+function toggle(arg) {
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach(block => {
+        if (arg === 'reset') {
+            block.style.backgroundColor = null;
+            block.classList.remove('active');
+            return;
+        }
+        block.addEventListener('mouseover', () => {
+            if (arg === 'erase') {
+                block.style.backgroundColor = null;
+                block.classList.remove('active');
+            }
+            else if (arg === 'random') {
+                block.style.backgroundColor= selectColor(Math.floor(Math.random() * 999));
+            }
+            else {
+                block.style.backgroundColor = null;
+                block.classList.add('active');
+            }
+        })
     })
-})
+}
+
+
+const btn = document.querySelector('#size');
+const draw = document.querySelector('#draw');
+const eraser = document.querySelector('#erase');
+const random = document.querySelector('#random');
+const reset = document.querySelector('#reset');
+
+btn.addEventListener('click', () => {
+    console.log('lol')
+    size = prompt('Enter number of squares on one side');
+    if (size > 100) {
+        alert("Max size can be 100.");
+        return;
+    }
+    if (size < 1) {
+        alert("Enter a valid value.");
+        return;
+    }
+    rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        row.parentNode.removeChild(row);
+    })
+    create(size);
+});
+
+eraser.addEventListener('click', toggle.bind(null, 'erase'));
+draw.addEventListener('click', toggle.bind(null, 'draw'));
+random.addEventListener('click', toggle.bind(null, 'random'));
+reset.addEventListener('click', toggle.bind(null, 'reset'));
+
+create();
